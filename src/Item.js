f1272db72d439c7d9cabfc34e2c4abffaab2dd24
naveen-ku <naveen2951996@gmail.com>
2019-10-09 12:36:19 +0530

    adding one more feature

--- src/Item.js
+++ src/Item.js
@@ -1,19 +1,23 @@
 import React from 'react'
 
 const Item = (props) =>{
+    const styles={
+            color :"blue",
+            background : "yellow",
+            fontStyle : "italic"}
+    
     return (
                 <div className="list">
                 <input
                     type="checkbox"  
                     checked={props.data.completed} 
-                    onChange={() => props.handleChange(props.data.id)}            
+                    onChange={() => props.handleChange(props.data.id)}         
                 />
-                {props.data.text}
+                <p style ={props.data.completed?styles:null}>{props.data.text}</p>
                 <br />
                 <hr />
 
                 </div>
     )
 }
-
 export default Item;
